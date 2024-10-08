c2fc35ea2ca9c245e80377aa17ebb8cd475f363b
anca26 <anca.iacob26@gmail.com>
2024-10-08 13:39:48 +0300

    mda

--- HW_1_1/Assets/Kawaii Slimes/Scripts/CharacterBehaviour.cs
+++ HW_1_1/Assets/Kawaii Slimes/Scripts/CharacterBehaviour.cs
@@ -5,6 +5,11 @@ public class CharacterBehavior : MonoBehaviour
     public Animator animator;
     public float detectionRadius = 0.25f; 
 
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     void Update()
     {
         Collider[] hitColliders = Physics.OverlapSphere(transform.position, detectionRadius);
