0c52829b43d074dca052c0b3d4bdc2f46a6d4f13
alinacaprioara <alinacaprioara13@gmail.com>
2024-10-09 09:22:56 +0300

    Small updates on Animator and Behavior script

--- HW_1_1/Assets/Kawaii Slimes/Scripts/CharacterBehaviour.cs
+++ HW_1_1/Assets/Kawaii Slimes/Scripts/CharacterBehaviour.cs
@@ -3,7 +3,17 @@ using UnityEngine;
 public class CharacterBehavior : MonoBehaviour
 {
     public Animator animator;
-    public float detectionRadius = 0.25f; 
+    public float detectionRadius = 0.25f;
+
+
+    void Start()
+    {
+        if (animator == null)
+        {
+            animator = GetComponent<Animator>();
+        }
+    }
+
 
     void Update()
     {
@@ -12,6 +22,8 @@ public class CharacterBehavior : MonoBehaviour
 
         foreach (var hitCollider in hitColliders)
         {
+            Debug.Log("Detected: " + hitCollider.gameObject.name);
+
             if (hitCollider.gameObject != gameObject) 
             {
                 isAttacking = true; 
@@ -21,11 +33,18 @@ public class CharacterBehavior : MonoBehaviour
 
         if (isAttacking)
         {
+            Debug.Log("Character is attacking");
             animator.SetBool("isAttacking", true);
         }
         else
         {
             animator.SetBool("isAttacking", false);
         }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, detectionRadius);
     }
 }
